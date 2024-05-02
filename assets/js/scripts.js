066af089644d3e024ded041c67b1e11adda00096
Bruno Wozniak <bruno.wozniak@gmail.com>
2024-05-02 16:12:27 +0200

    Added a testimonial section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,17 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[980,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
