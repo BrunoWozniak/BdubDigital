efc4dc39705904146b98803abe6add8693cc022b
Bruno Wozniak <bruno.wozniak@gmail.com>
2024-05-14 22:22:06 +0200

    Three testimonials (wow)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,8 +10,8 @@ menuTrigger.onclick = function() {
 
 $(document).ready(function(){
     $("#testimonial-slider").owlCarousel({
-        items:3,
-        itemsDesktop:[1000,3],
+        items:2,
+        itemsDesktop:[1000,2],
         itemsDesktopSmall:[980,2],
         itemsTablet:[768,2],
         itemsMobile:[650,1],
